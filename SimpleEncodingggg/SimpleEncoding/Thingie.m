8dab12aa1d5c37977209730997fa36f0178f96a7
LinHaotian <542518989@qq.com>
2020-09-19 09:31:51 +0800

    no message

--- SimpleEncodingggg/SimpleEncoding/Thingie.m
+++ SimpleEncodingggg/SimpleEncoding/Thingie.m
@@ -35,6 +35,23 @@
     [super dealloc];
 }
 
+- (void)encodeWithCoder:(NSCoder *)coder {
+    [coder encodeObject:name forKey:@"name"];
+    [coder encodeInt:magicNumber forKey:@"magicNumber"];
+    [coder encodeFloat:shoeSize forKey:@"shoeSize"];
+    [coder encodeObject:subThingies forKey:@"subThingies"];
+}
+
+- (id)initWithCoder:(NSCoder *)decoder {
+    if (self = [super init]) {
+        self.name = [decoder decodeObjectForKey:@"name"];
+        self.magicNumber = [decoder decodeIntForKey:@"magicNumber"];
+        self.shoeSize = [decoder decodeFloatForKey:@"shoeSize"];
+        self.subThingies = [decoder decodeObjectForKey:@"subThingies"];
+    }
+    return self;
+}
+
 - (NSString *)description {
     NSString *description = [NSString stringWithFormat:@"%@: %d/%.1f %@", name, magicNumber, shoeSize, subThingies];
     
